142589108f844eae6abf0c3d3baa51e5597d5edb
Carl Skyborn <c.skyborn@gmail.com>
2021-10-19 13:23:04 +0200

    fixat fler databas funcs

--- back-end/routes.js
+++ back-end/routes.js
@@ -3,12 +3,37 @@ const db = require("./database");
 
 
 routes.get("/", async (req, res) => {
-    try {
-    //const XXX = await db.XXX();
-    //res.send(XXX);
-    } catch (error) {
-      res.status(500).json({status: "nok"});
+  try {
+    if (req.query.city) {
+      const cities = await db.searchCity(req.query.city);
+      res.send(cities);
+    } else {
+      const cities = await db.getCities();
+      res.send(cities);
     }
-  });
+  } catch (error) {
+    res.status(500).json({status: "nok"});
+  }
+});
 
-  module.exports = routes;
+routes.get("/categories", async (req, res) => {
+  try {
+    const cats = await db.getCategories();
+    res.send(cats);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({status: "nok"});
+  }
+});
+
+routes.get("/:params", async (req, res) => {
+  try {
+    const acts = await db.getActivites();
+    res.send(acts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({status: "nok"});
+  }
+});
+
+module.exports = routes;
