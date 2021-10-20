a8b5f87b0752b793fbd9e25060f109da408c9a85
Hanna Mellin <hannahmellin@gmail.com>
2021-10-20 17:09:48 +0200

    La till tillbakaknapp till startsida

--- front-end/src/pages/Singlepage.js
+++ front-end/src/pages/Singlepage.js
@@ -1,11 +1,12 @@
 import { EventFetch } from "../components/EventFetch/EventFetch";
 import { Link } from "react-router-dom";
 
+
 export function SinglePage() {
 
   return(
     <>
-    <Link to='/'>Tillbaka till sökresultat</Link>
+    <Link to="/">Tillbaka</Link>
     <EventFetch />
     </>
   );
