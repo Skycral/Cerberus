cbc33dec8e22ff7561baac59540b584296fb39f7
Hanna Mellin <hannahmellin@gmail.com>
2021-10-20 17:02:39 +0200

    La in länk tillbaka till startsidan

--- front-end/src/pages/Singlepage.js
+++ front-end/src/pages/Singlepage.js
@@ -1,8 +1,12 @@
 import { EventFetch } from "../components/EventFetch/EventFetch";
+import { Link } from "react-router-dom";
 
 export function SinglePage() {
 
   return(
+    <>
+    <Link to='/'>Tillbaka till sökresultat</Link>
     <EventFetch />
+    </>
   );
 };
