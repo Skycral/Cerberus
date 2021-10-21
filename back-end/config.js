e679eaa0c397212492531d33dde3be5e2b4a2dcb
Hanna Mellin <hannahmellin@gmail.com>
2021-10-21 17:47:55 +0200

    Merge med oscars routes

--- back-end/config.js
+++ back-end/config.js
@@ -4,4 +4,5 @@ module.exports = {
   port: process.env.PORT,
   ticketmaster: process.env.TICKETMASTER_API_KEY,
   weatherKey: process.env.WEATHER_API_KEY,
+  googleKey: process.env.GOOGLE_API_KEY,
 };
